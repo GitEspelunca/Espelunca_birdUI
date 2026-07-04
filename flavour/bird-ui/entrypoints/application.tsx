461dc7d5fc9bc24f3cecad4a6c07b032e5944667
Espelunca <jensenhunterdbd@gmail.com>
2026-07-04 19:26:27 -0400

    Add: Bird UI flavour with complete SCSS architecture and locales

---
+++ flavour/bird-ui/entrypoints/application.tsx
@@ -0,0 +1,7 @@
+import { setupApplication } from 'flavours/bird-ui/setup';
+import { loadLocalization } from 'flavours/bird-ui/locales';
+
+// Initialize Bird UI
+setupApplication();
+
+export * from 'flavours/vanilla/entrypoints/application';
